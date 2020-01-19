e9ba30e1b6006c1c59b3920e134c3db7636d2c28
Colin Kennedy <colinvfx@gmail.com>
2020-01-19 14:40:25 -0800

    Changed package definition to have minimum Rez package versions

--- rez_utilities/package.py
+++ rez_utilities/package.py
@@ -20,29 +20,34 @@ private_build_requires = ["rez_build_helper-1+<2"]
 build_command = "python -m rez_build_helper --items python"
 
 tests = {
-    "black_diff": {"command": "rez-env black -- black --diff --check python tests"},
-    "black": {"command": "rez-env black -- black python tests"},
+    "black_diff": {"command": "rez-env black-19.10+ -- black --diff --check python tests"},
+    "black": {"command": "rez-env black-19.10+ -- black python tests"},
     "coverage": {
         "command": "coverage erase && coverage run --parallel-mode --include=python/* -m unittest discover && coverage combine --append && coverage html",
-        "requires": ["coverage"],
+        "requires": ["coverage-4.5+<5"],
     },
     "isort": {"command": "isort --recursive python tests", "requires": ["isort"]},
     "isort_check": {
         "command": "isort --check-only --diff --recursive python tests",
-        "requires": ["isort"],
+        "requires": ["isort-4.3+<5"],
     },
     "pydocstyle": {
         # Need to disable D202 for now, until a new pydocstyle version is released
         # Reference: https://github.com/psf/black/issues/1159
         #
-        "command": "rez-env pydocstyle -- pydocstyle --ignore=D213,D202,D203,D406,D407 python tests",
-        "requires": ["pydocstyle"],
+        "command": "pydocstyle --ignore=D213,D202,D203,D406,D407 python tests",
+        "requires": ["pydocstyle-3+"],
     },
     "pylint": {
         "command": "pylint --disable=bad-continuation python/rez_utilities tests",
         "requires": ["pylint-1.9+<2"],
     },
-    "unittest": "python -m unittest discover",
+    "unittest": {
+        "command": "python -m unittest discover",
+        "requires": [
+            "GitPython-3+<4",
+        ],
+    },
 }
 
 
