b4a275d3dc5b67d4a921ee0c932fac51f9cfe716
Colin Kennedy <colinvfx@gmail.com>
2020-01-19 14:41:22 -0800

    Added mock and WIP unittest

--- rez_utilities/tests/__init__.py
+++ rez_utilities/tests/__init__.py
@@ -1,1 +1,5 @@
 """A suite of tests for this Rez package."""
+
+import six
+
+six.add_move(six.MovedModule('mock', 'mock', 'unittest.mock'))
--- rez_utilities/tests/test_creator.py
+++ rez_utilities/tests/test_creator.py
@@ -3,15 +3,20 @@
 
 """A series of tests for building/installing Rez packages."""
 
+import logging
 import os
 import tempfile
 import textwrap
 
 from python_compatibility.testing import common
 from rez import exceptions, packages_
-from rez_utilities import creator
+from rez_utilities import creator, inspection
+from six.moves import mock
+import wurlitzer
+import git
 
 _CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+_LOGGER = logging.getLogger(__name__)
 
 
 class Build(common.Common):
@@ -66,6 +71,184 @@ class Build(common.Common):
             creator.build(package, root)
 
 
+class Release(common.Common):
+    """Make sure :func:`rez_utilities.creator.release_package` works as expected."""
+
+    def test_normal(self):
+        """Release a package and make sure it is valid."""
+        def _make_fake_git_repository_at_directory(root):
+            repository = git.Repo.init(root)
+            repository.index.add(".")
+            repository.index.commit("initial commit")
+
+        source_path = tempfile.mkdtemp(suffix="_rez_package_source_path")
+        self.add_item(source_path)
+
+        with open(os.path.join(source_path, "package.py"), "w") as handler:
+            handler.write(
+                textwrap.dedent(
+                    """\
+                    name = "some_package"
+                    version = "1.0.0"
+                    build_command = "echo 'foo'"
+                    """
+                )
+            )
+
+        _make_fake_git_repository_at_directory(source_path)
+
+        package = packages_.get_developer_package(source_path)
+
+        options = mock.MagicMock()
+        options.cmd = "release"
+        options.debug = False
+        options.message = "Fake release message"
+        options.no_message = False
+        options.process = "local"
+        options.variants = None
+        options.vcs = None
+
+        parser = mock.MagicMock()
+        parser.prog = "rez release"
+
+        release_path = tempfile.mkdtemp(suffix="_rez_package_release_path")
+        self.add_item(release_path)
+
+        with wurlitzer.pipes():
+            new_release_path = creator.release(
+                inspection.get_package_root(package),
+                options,
+                parser,
+                release_path,
+            )
+
+        self.assertIsNotNone(packages_.get_developer_package(new_release_path))
+        self.assertTrue(os.path.isfile(os.path.join(new_release_path, "some_packge", "1.0.0")))
+
+    # def test_normal(self):
+    #     """Release a package and make sure it is valid."""
+    #     source_path = tempfile.mkdtemp(suffix="_rez_package_source_path")
+    #     # self.add_item(source_path)
+    #
+    #     with open(os.path.join(source_path, "package.py"), "w") as handler:
+    #         handler.write(
+    #             textwrap.dedent(
+    #                 """\
+    #                 name = "some_package"
+    #                 version = "1.0.0"
+    #                 build_command = "echo 'foo'"
+    #                 """
+    #             )
+    #         )
+    #
+    #     package = packages_.get_developer_package(source_path)
+    #
+    #     options = mock.MagicMock()
+    #     options.cmd = "release"
+    #     options.debug = False
+    #     options.message = "Fake release message"
+    #     options.no_message = False
+    #     options.process = "local"
+    #     options.variants = None
+    #     options.vcs = None
+    #
+    #     parser = mock.MagicMock()
+    #     parser.prog = "rez release"
+    #
+    #     release_path = tempfile.mkdtemp(suffix="_rez_package_release_path")
+    #     # self.add_item(release_path)
+    #
+    #     # Rez prints a lot of text to console during release so we'll silence it all
+    #     with mock.patch('rez.release_vcs.create_release_vcs') as patch:
+    #         type(patch.return_value).pkg_root = inspection.get_package_root(package)
+    #         type(patch.return_value).get_changelog = lambda x, y, max_revisions: "asdfasd"
+    #
+    #         new_release_path = creator.release(
+    #             inspection.get_package_root(package),
+    #             options,
+    #             parser,
+    #             release_path,
+    #         )
+    #
+    #     self.assertIsNotNone(packages_.get_developer_package(new_release_path))
+    #     self.assertTrue(os.path.isfile(os.path.join(new_release_path, "some_packge", "1.0.0")))
+
+
+# def _release_packages(packages, search_paths=None):
+#     """Release 1+ Rez packages.
+#
+#     Args:
+#         packages (list[:class:`rez.packages_.Package`]):
+#             The source Rez packages that will be built and released.
+#         search_paths (list[str], optional):
+#             The directories on-disk that can be used to help find extra
+#             dependencies that a Rez package might require. Default is None.
+#
+#     Raises:
+#         ValueError: If `packages` is invalid.
+#
+#     Returns:
+#         str:
+#             The directory where the released package was sent to.
+#             Normally, this should always be `new_release_path`. But
+#             if `new_release_path` wasn't given then this returns a
+#             temporary directory.
+#
+#     """
+#     if not packages:
+#         raise ValueError("No packages were given")
+#
+#     if not search_paths:
+#         search_paths = []
+#
+#     options = mock.MagicMock()
+#     options.cmd = "release"
+#     options.debug = False
+#     options.message = "Fake release message"
+#     options.no_message = False
+#     options.process = "local"
+#     options.variants = None
+#     options.vcs = None
+#
+#     parser = mock.MagicMock()
+#     parser.prog = "rez release"
+#
+#     package = packages[0]
+#
+#     _LOGGER.info(
+#         'Releasing the fake package "%s" to a temporary directory.', package.name
+#     )
+#
+#     release_path = tempfile.mkdtemp()
+#
+#     # Rez prints a lot of text to console during release so we'll silence it all
+#     with wurlitzer.pipes():
+#         new_release_path = creator.release(
+#             inspection.get_package_root(package),
+#             options,
+#             parser,
+#             release_path,
+#             search_paths=search_paths,
+#         )
+#
+#     for package in packages[1:]:
+#         with wurlitzer.pipes():
+#             _LOGGER.info(
+#                 'Releasing the fake package "%s" to "%s".',
+#                 package.name,
+#                 new_release_path,
+#             )
+#             creator.release(
+#                 inspection.get_package_root(package),
+#                 options,
+#                 parser,
+#                 new_release_path,
+#                 search_paths=search_paths,
+#             )
+#
+#     return new_release_path
+#
+#
 def _build_source_that_has_build_method(root):
     """Create a source Rez package and then build it to some temporary location."""
     with open(os.path.join(root, "package.py"), "w") as handler:
