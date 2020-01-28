ccd3db438ea8100488c72620a9572856e21a5375
Colin Kennedy <colinvfx@gmail.com>
2020-01-27 21:41:58 -0800

    Added `fix_intersphinx_mapping` to api.py

--- rez_documentation_check/python/rez_documentation_check/api.py
+++ rez_documentation_check/python/rez_documentation_check/api.py
@@ -5,12 +5,14 @@
 
 from .cli import (
     find_intersphinx_links,
+    fix_intersphinx_mapping,
     get_existing_intersphinx_links,
     get_missing_intersphinx_mappings,
 )
 
 __all__ = [
     "find_intersphinx_links",
+    "fix_intersphinx_mapping",
     "get_existing_intersphinx_links",
     "get_missing_intersphinx_mappings",
 ]
