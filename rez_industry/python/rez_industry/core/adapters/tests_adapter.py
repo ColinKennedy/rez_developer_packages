fff21d1c6fde5d523e2ff6786a557587747351ed
Colin Kennedy <colinvfx@gmail.com>
2020-02-01 15:55:23 -0800

    Adding docstrings

--- rez_industry/python/rez_industry/core/adapters/tests_adapter.py
+++ rez_industry/python/rez_industry/core/adapters/tests_adapter.py
@@ -91,7 +91,7 @@ class TestsAdapter(base_.BaseAdapter):
 
         if assignment:
             maker_root = _get_dict_maker_root(assignment)
-            _flatten_nodes(maker_root)
+            maker_root = _flatten_nodes(maker_root)
 
         existing = _get_tests_data(graph)
         new = dict()
@@ -124,7 +124,7 @@ def _is_empty_dict(node):
     """Check if the given parso `node` represents an initialized-but-empty assignment.
 
     Args:
-        node (:class:`parso.python.Tree.ExprStmt`): The assignment node to check.
+        node (:class:`parso.python.tree.ExprStmt`): The assignment node to check.
 
     Returns:
         bool: Return False if `node` is a non-empty dict. Otherwise, return True.
@@ -173,9 +173,9 @@ def _is_parso_dict_instance(value):
     This function is trying to define the outer "atom" PythonNode.
 
     Args:
-        value (:class:`parso.python.Tree.PythonBaseNode`):
+        value (:class:`parso.python.tree.PythonBaseNode`):
             A Parso node that needs to be checked. Only a
-            :class:`parso.python.Tree.PythonNode` has a possibility of
+            :class:`parso.python.tree.PythonNode` has a possibility of
             returning True (but even then it is not guaranteed).
 
     Returns:
@@ -201,7 +201,7 @@ def _get_dict_maker_root(node):
     """Find the inner parso PythonNode that defines a Python dict.
 
     Args:
-        node (:class:`parso.python.Tree.PythonBaseNode`):
+        node (:class:`parso.python.tree.PythonBaseNode`):
             A parso node that may or may not have a "dictorsetmaker"
             node as one of its children. Usually, this will actually be
             an "atom" type :class:`parso.python.tree.PythonNode`
@@ -220,6 +220,18 @@ def _get_dict_maker_root(node):
 
 
 def _get_dict_maker_pairs(node):
+    """Get the key / value dict information from some parso node.
+
+    Args:
+        node (:class:`parso.python.tree.PythonNode`):
+            The parso node that is the "inner-most" parso node, closest
+            to the key / value pairs of a Python dict.
+
+    Returns:
+        list[tuple[:class:`parso.python.tree.PythonBaseNode`, :class:`parso.python.tree.PythonBaseNode`]]:
+            The found parso key / value pairs of `node`.
+
+    """
     operators = []
 
     for index, child in enumerate(node.children):
@@ -230,6 +242,26 @@ def _get_dict_maker_pairs(node):
 
 
 def _get_tests_data(graph):
+    """Get all of the existing key / value information of a "tests" attribute.
+
+    It's assumed that `graph` defines (or at least includes) a Rez
+    package.py's contents.
+
+    Args:
+        graph (:class:`parso.python.tree.PythonBaseNode`):
+            The root node that will be checked. Usually, this is a
+            :class:`parso.python.tree.Module` but it doesn't have to be.
+
+    Returns:
+        dict[
+            str,
+            :class:`parso.python.tree.PythonBaseNode`
+                or dict[str, :class:`parso.python.tree.PythonBaseNode`]
+            ]:
+            The existing tests data. If `graph` doesn't have assignment
+            data then an empty dict is returned.
+
+    """
     graph = copy.deepcopy(graph)
 
     try:
@@ -268,13 +300,13 @@ def _make_makeshift_node_dict(node):
     """Find every key / value of a parso dict and partially cast it back to a Python object.
 
     Args:
-        node (:class:`parso.python.Tree.PythonBaseNode`):
+        node (:class:`parso.python.tree.PythonBaseNode`):
             A parso node that may or may not have a "dictorsetmaker"
             node as one of its children. Usually, this will actually be
             an "atom" type :class:`parso.python.tree.PythonNode`
 
     Returns:
-        dict[str, :class:`parso.python.Tree.PythonBaseNode`]:
+        dict[str, :class:`parso.python.tree.PythonBaseNode`]:
             The original `node`, but converted into actual Python keys.
 
     """
@@ -284,8 +316,27 @@ def _make_makeshift_node_dict(node):
     return {key.value.strip("'\""): value for key, value in data_pairs}
 
 
-# TODO : Change the name
-def _update_foo(existing, extras):
+# TODO : Add a unittest that changes a string into an inner dict. Very important!
+def _update_partial_python_dict(existing, extras):
+    """Add the contents of `extras` onto `existing`.
+
+    If `existing` represents a dict-of-dicts, and an "inner" dict has
+    a key that `extras` does not, that key is preserved. If both dicts
+    have the same key `extras` is preferred.
+
+    Reference:
+        https://stackoverflow.com/a/59685868/3626104
+
+    Args:
+        existing (dict[str, :class:`parso.python.tree.PythonBaseNode`]):
+            Some data that will be overwritten by `extras`.
+        extras (dict[str, :class:`parso.python.tree.PythonBaseNode`]):
+            Data to overlay onto `existing`.
+
+    Returns:
+        dict[str, :class:`parso.python.tree.PythonBaseNode`]: The merged data.
+
+    """
     if not isinstance(existing, collections.MutableMapping):
         return extras
 
@@ -294,7 +345,7 @@ def _update_foo(existing, extras):
     for key, value in extras.items():
         if _is_parso_dict_instance(value):
             dict_value = _make_makeshift_node_dict(value)
-            existing[key] = _update_foo(existing.get(key, dict()), dict_value)
+            existing[key] = _update_partial_python_dict(existing.get(key, dict()), dict_value)
         else:
             existing[key] = value
 
@@ -302,21 +353,45 @@ def _update_foo(existing, extras):
 
 
 def _override_tests(base, data):
+    """Add `data` onto `base`, while prioritizing anything in `data` over `base`.
+
+    Args:
+        base (dict[str, :class:`parso.python.tree.PythonBaseNode`]):
+            The existing data that comes from a Rez package.py file.
+            Since the "tests" attribute of a Rez package is always a
+            dict, `base` will always be a dict of key/value pairs.
+        data (dict[str, str] or dict[str, dict[str, str or list]]):
+            Any information that will be added over top of `base`.
+
+    Returns:
+        dict[str, :class:`parso.python.tree.PythonBaseNode`]: The merged data.
+
+    """
     if not base:
         return data
 
     data_graph = parso.parse(json.dumps(data))
-    _flatten_nodes(data_graph)
+    data_graph = _flatten_nodes(data_graph)
     data_pairs = _make_makeshift_node_dict(data_graph)
 
-    return _update_foo(base, data_pairs)
+    return _update_partial_python_dict(base, data_pairs)
 
 
 def _escape(key):
-    return '"{key}"'.format(key=key.replace('"', '"'))
+    """Make some text into a double-quoted dict key string."""
+    return '"{key}"'.format(key=key.replace('"', '\"'))
 
 
 def _escape_all(value):
+    """Convert `value` into a string.
+
+    Args:
+        value (object): Some parso or Python object that needs to be made into a string.
+
+    Returns:
+        str: A string representation of `value`.
+
+    """
     if hasattr(value, "get_code"):
         return value.get_code()
 
@@ -330,6 +405,23 @@ def _escape_all(value):
 
 
 def _make_dict_nodes(data, prefix=""):
+    """Create a parso node that represents the inner key / value pairs of a Python dict.
+
+    In terms of the work needed to generate a Rez "tests" attribute,
+    this function is used mainly to create the "inner dict" that a
+    "tests" attribute frequently defines.
+
+    Args:
+        data (iter[tuple[str, str or list or :class:`parso.python.tree.PythonBaseNode`]]):
+
+    Returns:
+        :class:`parso.python.tree.PythonNode`:
+            A node that defines the inside of a parso dict. This node
+            is not equivalent to an actual dict and still needs to be
+            wrapped with another :class:`parso.python.tree.PythonNode`
+            to be valid.
+
+    """
     nodes = []
 
     for key, item in data:
@@ -361,13 +453,59 @@ def _make_dict_nodes(data, prefix=""):
     )
 
 
-def _flatten_nodes(data_graph):
-    for child in parso_helper.iter_nested_children(data_graph):
+def _flatten_nodes(node):
+    """Remove all prefix / whitespace information from a parso node's children.
+
+    :class:`TestsAdapter` is responsible for setting + appending
+    whitespace for all of the parso nodes that eventually get written
+    to disk. But existing data may already have whitespace prefix data.
+    So this function sets that all back to zero, to make it easier to
+    append a prefix to existing nodes.
+
+    Args:
+        node (:class:`parso.python.tree.PythonBaseNode`):
+            A parso object that presumably has child nodes. These
+            children may have whitespace - every child's whitespace will
+            be removed.
+
+    Returns:
+        :class:`parso.python.tree.PythonBaseNode`:
+            A copy of `node` whose children have no defined whitespace.
+
+    """
+    node = copy.deepcopy(node)
+
+    for child in parso_helper.iter_nested_children(node):
         if hasattr(child, "prefix"):
             child.prefix = ""
 
+    return node
+
 
 def _make_tests_node(data):
+    """Create the final "tests" data that will be written to a package.py file.
+
+    Important:
+        This function technically doesn't create a "correct"
+        parso dict. For the sake of simplicity, some corners were
+        cut. For example, value data is converted into a raw
+        :class:`parso.python.tree.String`. It's not technically
+        "correct" but since this function is called just before writing
+        the data into disk and not further modified, it's okay.
+
+    Args:
+        data (iter[str, :class:`parso.python.tree.PythonBaseNode`]):
+            The key / value information that will be converted into a
+            parso dict.
+
+    Returns:
+        :class:`parso.python.tree.PythonNode`:
+            An atom PythonNode which has a full dictionary "tests"
+            defined. This will later override an existing "tests"
+            attribute in a package.py or be appended to the file as an
+            assignment.
+
+    """
     nodes = []
 
     for key, value in data:
