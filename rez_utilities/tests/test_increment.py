fd121560393e281abb2f6912f4cc864ff87d9b3d
Colin Kennedy <colinvfx@gmail.com>
2020-02-16 19:40:55 -0800

    Added WIP unittests

--- rez_utilities/tests/test_increment.py
+++ rez_utilities/tests/test_increment.py
@@ -1,25 +1,91 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import tempfile
+import textwrap
 
-import unittest
+from python_compatibility.testing import common
 
 
-class Version(unittest.TestCase):
+class Version(common.Common):
     def test_increment_minor(self):
-        pass
+        """Add a new value to a package's version without knowing what that version is."""
+        directory = tempfile.mkdtemp(suffix="_test_increment_minor")
+        self.delete_item_later(directory)
+
+        with open(os.path.join(directory, "package.py"), "w") as handler:
+            handler.write(
+                textwrap.dedent(
+                    """\
+                    """
+                )
+            )
 
     def test_decrement_minor(self):
-        pass
+        """Lower the value of a package's version without knowing what that version is."""
+        directory = tempfile.mkdtemp(suffix="_test_decrement_minor")
+        self.delete_item_later(directory)
+
+        with open(os.path.join(directory, "package.py"), "w") as handler:
+            handler.write(
+                textwrap.dedent(
+                    """\
+                    """
+                )
+            )
 
     def test_absolute_increment_minor(self):
-        pass
+        directory = tempfile.mkdtemp(suffix="_test_absolute_increment_minor")
+        self.delete_item_later(directory)
+
+        with open(os.path.join(directory, "package.py"), "w") as handler:
+            handler.write(
+                textwrap.dedent(
+                    """\
+                    """
+                )
+            )
 
     def test_absolute_decrement_minor(self):
-        pass
+        directory = tempfile.mkdtemp(suffix="_test_absolute_decrement_minor")
+        self.delete_item_later(directory)
+
+        with open(os.path.join(directory, "package.py"), "w") as handler:
+            handler.write(
+                textwrap.dedent(
+                    """\
+                    """
+                )
+            )
 
     def test_complex_details(self):
-        pass
+        """Write a really complex package.py file out, reliably, with a new version."""
+        directory = tempfile.mkdtemp(suffix="_test_complex_details")
+        self.delete_item_later(directory)
+
+        with open(os.path.join(directory, "package.py"), "w") as handler:
+            handler.write(
+                textwrap.dedent(
+                    """\
+                    """
+                )
+            )
 
     def test_format_yaml(self):
+        directory = tempfile.mkdtemp(suffix="_test_format_yaml")
+        self.delete_item_later(directory)
+
+        with open(os.path.join(directory, "package.py"), "w") as handler:
+            handler.write(
+                textwrap.dedent(
+                    """\
+                    """
+                )
+            )
+
+    def test_invalid_001(self):
+        pass
+
+    def test_invalid_002(self):
         pass
