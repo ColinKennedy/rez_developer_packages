b4cb166c299034cf61612c8efc1b8668e0849711
Colin Kennedy <colinvfx@gmail.com>
2020-04-09 18:19:20 -0700

    Updated move_break to 2+<3

--- rez_move_imports/package.py
+++ rez_move_imports/package.py
@@ -2,7 +2,7 @@
 
 name = "rez_move_imports"
 
-version = "1.0.0"
+version = "1.1.0"
 
 description = "Change a Rez package's imports and then bump the require Rez version(s)"
 
@@ -13,7 +13,7 @@ help = [["README", "README.md"]]
 private_build_requires = ["rez_build_helper-1+<2"]
 
 requires = [
-    "move_break-1.1+<2",
+    "move_break-2+<3",
     "python-2+<3",
     "python_compatibility-2.1+<3",
     "rez-2.42+<3",
