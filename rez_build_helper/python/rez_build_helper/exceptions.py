c09fa474ca0a2a18079123e606cd22c79b1e06d1
Colin Kennedy <colinvfx@gmail.com>
2020-06-19 00:39:47 -0700

    Added missing exceptions.py

---
+++ rez_build_helper/python/rez_build_helper/exceptions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":mod:`rez_build_helper` specific exceptions for CLI use."""
+
+
+class NonRootItemFound(Exception):
+    """Only root file(s)/folder(s) may be converted into .egg files."""
+
+    pass
