7d00a046b68649ca36ab0fcd98f9b5b18b528b01
Colin Kennedy <colinvfx@gmail.com>
2020-11-28 13:18:03 -0800

    Added rez-test commands

--- parso_helper/package.py
+++ parso_helper/package.py
@@ -12,14 +12,43 @@ authors = [
 
 private_build_requires = ["rez_build_helper-1+<2"]
 
-requires = [
-    "parso-0.5+<1",
-    "python-2",
-]
+requires = ["parso-0.5+<1", "python-2+<3.8"]
 
 build_command = "python -m rez_build_helper --items python"
 
 uuid = "c989f6ef-7f52-4acf-8660-e0268049f700"
+
+tests = {
+    "unittest": "python -m unittest discover",
+    "isort": {
+        "command": "isort --recursive package.py python tests",
+        "requires": ["isort-4.3+<5"],
+        "run_on": "explicit",
+    },
+    "isort_check": {
+        "command": "isort --check-only --diff --recursive package.py python tests",
+        "requires": ["isort-4.3+<5"],
+    },
+    "pydocstyle": {
+        # Need to disable D202 for now, until a new pydocstyle version is released
+        # Reference: https://github.com/psf/black/issues/1159
+        #
+        "command": "pydocstyle --ignore=D213,D202,D203,D406,D407 python tests/*",
+        "requires": ["pydocstyle-3+<5"],
+    },
+    "pylint": {
+        "command": "pylint --disable=bad-continuation python/move_break tests",
+        "requires": ["pylint-1.9+<2"],
+    },
+    "unittest_python_2": {
+        "command": "python -m unittest discover",
+        "requires": ["python-2"],
+    },
+    "unittest_python_3": {
+        "command": "python -m unittest discover",
+        "requires": ["python-3.6+<3.8"],
+    },
+}
 
 
 def commands():
