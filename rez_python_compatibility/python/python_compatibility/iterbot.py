6895512392ad62f3787557d1640abadfd7281cac
Colin Kennedy <colinvfx@gmail.com>
2020-12-08 00:10:49 -0800

    Adding functions to iterbot.py

--- rez_python_compatibility/python/python_compatibility/iterbot.py
+++ rez_python_compatibility/python/python_compatibility/iterbot.py
@@ -3,6 +3,8 @@
 
 """Generic functions that help iterating over containers and other iterables."""
 
+from six import moves
+
 
 def iter_is_last(container):
     """Iterate over a Python object and check if iterator's at the last element.
@@ -48,3 +50,84 @@ def iter_is_last(container):
 
     # Report the last value.
     yield True, last
+
+
+def iter_sub_finder(subseq, seq):
+    # Reference: https://stackoverflow.com/a/60819519
+    n = len(seq)
+    m = len(subseq)
+
+    if m == 0:
+        raise ValueError('Sub-sequence cannot be empty.')
+
+    stop = n - m + 1
+
+    if n > 0:
+        item = subseq[0]
+        i = 0
+        try:
+            while i < stop:
+                i = seq.index(item, i)
+                if seq[i:i + m] == subseq:
+                    yield i
+                i += 1
+        except ValueError:
+            return
+
+
+def make_chains(sequence, size=2):
+    """Get each sequential, overlapping chain, from a sequence.
+
+    Example:
+        >>> make_chains([0, 1, 2])
+        # [(0, 1), (1, 2)]
+
+    Args:
+        sequence (container[object]):
+            Any container of objects, as long as it supports `len()`.
+        size (int, optional):
+            The spacing for the chain. You can make "wider" chains but setting a higher size value. Default: 2.
+
+    Raises:
+        ValueError: If `sequence` cannot be chained or if `size` is not greater than 1.
+
+    Returns:
+        list[object]: The generated chain.
+
+    """
+    if size < 1:
+        raise ValueError('Size "{size}" cannot be less than 1.'.format(size=size))
+
+    try:
+        length = len(sequence)
+    except TypeError:
+        raise ValueError('Item "{sequence}" has no length.'.format(sequence=sequence))
+
+    return [sequence[start : start + size] for start in range(length - size + 1)]
+
+
+def make_pairs(sequence):
+    """Get unique pairs from `sequence`.
+
+    This function differs from :func:`make_chains` in 2 ways
+
+    1. :func:`make_chains` has duplicate values. This function does not
+    2. This function will always return pairs.
+        - Odd-length containers will have its last index dropped.
+
+    Args:
+        sequence (iter[object]): Some object to generate pairs from.
+
+    Raises:
+        ValueError: If `sequence` isn't able to be paired.
+
+    Returns:
+        list[object]: The created pairs.
+
+    """
+    try:
+        iterable = iter(sequence)
+    except TypeError:
+        raise ValueError('Item "{sequence}" is not iterable.'.format(sequence=sequence))
+
+    return moves.zip(iterable, iterable)
--- rez_python_compatibility/tests/test_iterbot.py
+++ rez_python_compatibility/tests/test_iterbot.py
@@ -58,3 +58,96 @@ class IterIsLast(unittest.TestCase):
             [(False, "another"), (False, "jpeg"), (True, "many")],
             list(iterbot.iter_is_last(["another", "jpeg", "many"])),
         )
+
+
+class IterSubFinder(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(
+            [],
+            list(iterbot.iter_sub_finder([4, 5], []))
+        )
+
+        with self.assertRaises(ValueError):
+            list(iterbot.iter_sub_finder([], [range(10)]))
+
+    def test_find(self):
+        self.assertEqual(
+            [4],
+            list(iterbot.iter_sub_finder([4, 5], list(range(10))))
+        )
+
+    def test_multiple_find_001(self):
+        self.assertEqual(
+            [4, 14],
+            list(iterbot.iter_sub_finder(
+                [4, 5],
+                list(range(10)) + list(range(10)),
+            ))
+        )
+
+class MakeChains(unittest.TestCase):
+    def test_custom(self):
+        self.assertEqual(
+            [range(0, 4), range(1, 5)],
+            list(iterbot.make_chains(range(5), size=4)),
+        )
+
+    def test_default(self):
+        self.assertEqual(
+            [range(0, 2), range(1, 3), range(2, 4), range(3, 5)],
+            list(iterbot.make_chains(range(5))),
+        )
+
+    def test_empty(self):
+        self.assertEqual(
+            [],
+            list(iterbot.make_chains([], size=4)),
+        )
+
+    def test_invalid(self):
+        """Make sure invalid values fail execution."""
+        with self.assertRaises(ValueError):
+            list(iterbot.make_chains(None, size=1))
+
+        with self.assertRaises(ValueError):
+            list(iterbot.make_chains([], size=-1))
+
+    def test_one(self):
+        self.assertEqual(
+            [range(0, 1)],
+            list(iterbot.make_chains(range(1), size=1)),
+        )
+
+    def test_string(self):
+        self.assertEqual(
+            ["abc", "bcd", "cde", "def", "efg"],
+            list(iterbot.make_chains("abcdefg", size=3)),
+        )
+
+    def test_too_high(self):
+        length = 5
+        self.assertEqual(
+            [],
+            list(iterbot.make_chains(range(length), size=length + 1)),
+        )
+
+
+class MakePairs(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual([], list(iterbot.make_pairs([])))
+
+    def test_invalid(self):
+        with self.assertRaises(ValueError):
+            list(iterbot.make_pairs(None))
+
+    def test_many(self):
+        self.assertEqual(
+            [(0, 1), (2, 3), (4, 5), (6, 7), (8, 9)],
+            list(iterbot.make_pairs(range(10))),
+        )
+
+    def test_odd(self):
+        self.assertEqual(
+            [(0, 1), (2, 3), (4, 5), (6, 7)],
+            list(iterbot.make_pairs(range(9))),
+        )
