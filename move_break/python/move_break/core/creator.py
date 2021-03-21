089c6a671715ec7c6afbd2ad6fb46d4782b8607e
Colin Kennedy <colinvfx@gmail.com>
2021-03-20 19:14:54 -0700

    Moved function and added make_name_node

--- move_break/python/move_break/core/creator.py
+++ move_break/python/move_break/core/creator.py
@@ -7,6 +7,53 @@ import six
 from parso.python import tree
 
 
+def make_from_import_using_parts(base, tail, prefix=""):
+    """Create a `from X import Y` object.
+
+    Args:
+        base (list[str] or str):
+            The `from X` part of the import.
+        tail (str):
+            The `import Y` part of the import.
+        prefix (str, optional):
+            Leading whitespace to add to the beginning of the
+            import. Default: "".
+
+    Returns:
+        :class:`parso.python.tree.PythonNode`: The created node.
+
+    """
+    import_base = base
+
+    if isinstance(base, six.string_types):
+        base_nodes = [tree.Name(part, (0, 0)) for part in base.split(".")]
+
+        for index in reversed(range(1, len(base_nodes), 1)):
+            base_nodes.insert(index, tree.Operator(".", (0, 0)))
+
+        base_nodes[0].prefix = " "
+        import_base = tree.PythonNode("dotted_name", base_nodes)
+
+    import_tail = tail
+
+    if isinstance(tail, six.string_types):
+        import_tail = tree.Name(tail, (0, 0), prefix=" ")
+
+    return tree.PythonNode(
+        "simple_stmt",
+        [
+            tree.ImportFrom(
+                [
+                    tree.Keyword("from", (0, 0), prefix=prefix),
+                    import_base,
+                    tree.Keyword("import", (0, 0), prefix=" "),
+                    import_tail,
+                ]
+            ),
+        ],
+    )
+
+
 def make_import(name, prefix=""):
     """Make a regular `import X` import.
 
@@ -53,48 +100,23 @@ def make_import_from_namespace(namespace):
     return make_from_import_using_parts(base, tail)
 
 
-def make_from_import_using_parts(base, tail, prefix=""):
-    """Create a `from X import Y` object.
+def make_name_node(namespace, prefix=None):
+    if "." not in namespace:
+        return tree.Name(namespace, (0, 0), prefix=prefix)
 
-    Args:
-        base (list[str] or str):
-            The `from X` part of the import.
-        tail (str):
-            The `import Y` part of the import.
-        prefix (str, optional):
-            Leading whitespace to add to the beginning of the
-            import. Default: "".
+    nodes = [tree.Name(name, (0, 0)) for name in namespace.split(".")]
+    nodes[0].prefix = prefix
+    children = []
 
-    Returns:
-        :class:`parso.python.tree.PythonNode`: The created node.
+    for element in nodes:
+        children.append(element)
+        children.append(tree.Operator(".", (0, 0)))
 
-    """
-    import_base = base
+    children.pop()
 
-    if isinstance(base, six.string_types):
-        base_nodes = [tree.Name(part, (0, 0)) for part in base.split(".")]
+    parent = tree.PythonNode("power", children)
 
-        for index in reversed(range(1, len(base_nodes), 1)):
-            base_nodes.insert(index, tree.Operator(".", (0, 0)))
+    for child in parent.children:
+        child.parent = parent
 
-        base_nodes[0].prefix = " "
-        import_base = tree.PythonNode("dotted_name", base_nodes)
-
-    import_tail = tail
-
-    if isinstance(tail, six.string_types):
-        import_tail = tree.Name(tail, (0, 0), prefix=" ")
-
-    return tree.PythonNode(
-        "simple_stmt",
-        [
-            tree.ImportFrom(
-                [
-                    tree.Keyword("from", (0, 0), prefix=prefix),
-                    import_base,
-                    tree.Keyword("import", (0, 0), prefix=" "),
-                    import_tail,
-                ]
-            ),
-        ],
-    )
+    return parent
