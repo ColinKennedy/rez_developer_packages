ef2a1976b633b929a94198954fb167db19292037
Colin Kennedy <colinvfx@gmail.com>
2021-08-01 17:17:34 -0700

    Adding basic bisect support

--- rez_bisect/python/rez_bisect/core/runner_.py
+++ rez_bisect/python/rez_bisect/core/runner_.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import random
 import math
 import subprocess
 
@@ -12,11 +13,93 @@ _NEWER = "newer_packages"
 _SUPPORTED_KEYS = frozenset((_NEWER, ))
 
 
-def _get_full_context(context, diff):
-    raise ValueError()
+def _get_full_context(bad, diff, command):
+    _validate_keys(diff)
+
+    newer_packages = diff.get(_NEWER)
+    keys = set(newer_packages.keys())
+    package_selection = random.sample(keys, (len(newer_packages) // 2) + 1)
+    bad_packages = {variant.name: variant for variant in bad.resolved_packages}
+
+    for name in package_selection:
+        working_package = newer_packages[name][0]
+        bad_packages[name] = working_package
+
+    context = resolved_context.ResolvedContext(
+        _to_request(bad_packages.values()),
+        package_paths=bad.package_paths,
+    )
+
+    if not _check_command(context, command):
+        # TODO : Add support for this later
+        raise NotImplementedError('Need to make a while loop for this part.')
+
+    raise ValueError('got this far')
 
 
 def _get_partial_context(context, diff, command):
+    _validate_keys(diff)
+
+    weight = 0.5
+    newer_packages = diff.get(_NEWER)
+    newer = newer_packages.values()
+    previous = None
+
+    while True:
+        newer_indices = dict()
+        request = []
+
+        for packages in newer:
+            index = math.floor(len(packages) * weight)
+            package = packages[index]
+            request.append(package)
+            newer_indices[package.name] = index
+
+        checker_context = resolved_context.ResolvedContext(
+            _to_request(request),
+            package_paths=context.package_paths,
+        )
+
+        if not _check_command(checker_context, command):
+            weight /= 2
+
+            continue
+
+        if request == previous:
+            break
+        else:
+            weight *= 1.5
+            previous = request
+
+    closest_newer = {key: value + 1 for key, value in newer_indices.items()}
+    request = [newer_packages[key][index] for key, index in closest_newer.items()]
+
+    return resolved_context.ResolvedContext(
+        _to_request(request),
+        package_paths=context.package_paths,
+    )
+
+
+def _check_command(context, command):
+    process = context.execute_command(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    process.communicate()
+
+    return process.returncode == 0
+
+
+def _to_request(request):
+    return [
+        "{package.name}=={package.version}".format(package=package)
+        for package in request
+    ]
+
+
+def _validate_keys(diff):
     keys = set(diff.keys())
     unsupported = keys - _SUPPORTED_KEYS
 
@@ -27,62 +110,6 @@ def _get_partial_context(context, diff, command):
             )
         )
 
-    weight = 0.5
-    newer = diff.get(_NEWER).values()
-    previous = None
-
-    while True:
-        package_indices = dict()
-        requests = []
-
-        for packages in newer:
-            index = math.floor(len(packages) * weight)
-            package = packages[index]
-            requests.append(package)
-            package_indices[package.name] = index
-
-        context = resolved_context.ResolvedContext(
-            [
-                "{package.name}=={package.version}".format(package=package)
-                for package in requests
-            ],
-            package_paths=context.package_paths,
-        )
-
-        process = context.execute_command(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        process.communicate()
-
-        print('RET', process.returncode, weight)
-
-        if process.returncode == 0:  # Success
-            raise NotImplementedError(requests)
-        else:
-            weight /= 2
-
-        if requests == previous:
-            break
-        else:
-            previous = requests
-
-    raise ValueError('Got this far')
-
-
-def _group_by_family(diff):
-    output = dict()
-
-    for key, data in diff.items():
-        output.setdefault(key, dict())
-
-        for package in packages:
-            output[key].setdefault(package.name, [])
-            output[key][package.name].append(package)
-
-        raise ValueError(output)
-
 
 def run(bad, command, good=None):
     if not good:
@@ -90,7 +117,7 @@ def run(bad, command, good=None):
         raise NotImplementedError("Need to write this")
 
     partial_context = _get_partial_context(good, good.get_resolve_diff(bad), command)
-    # full_context = _get_full_context(partial_context, partial_context.get_resolve_diff(bad))
+    full_context = _get_full_context(partial_context, good.get_resolve_diff(partial_context), command)
 
     # raise ValueError(full_context)
     # process = full_context.execute_shell(command)
