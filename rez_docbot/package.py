cda62fbe52b6d1becb8e5b812d77e128751e5f3e
Colin Kennedy <colinvfx@gmail.com>
2021-11-25 08:33:15 -0800

    Adding WIP Rez tests

--- rez_docbot/package.py
+++ rez_docbot/package.py
@@ -15,8 +15,54 @@ requires = [
     "six-1.16+<2",
 ]
 
+tests = {
+    "black_diff": {
+        "command": "black --diff --check package.py python tests",
+        "requires": ["black-11+<12"],  # TODO : Double-check version
+    },
+    "black": {
+        "command": "black package.py python tests",
+        "requires": ["black-11+<12"],  # TODO : Double-check version
+        "run_on": "explicit",
+    },
+    "isort": {
+        "command": "isort --recursive package.py python tests",
+        "requires": ["isort-4.3+<5"],
+        "run_on": "explicit",
+    },
+    "isort_check": {
+        "command": "isort --check-only --diff --recursive package.py python tests",
+        "requires": ["isort-4.3+<5"],
+    },
+    "pydocstyle": {
+        "command": "pydocstyle python tests",
+        "requires": ["pydocstyle-4+<5"],  # TODO : Add version number
+    },
+    "pydocstyle": {
+        # Need to disable D202 for now, until a new pydocstyle version is released
+        # Reference: https://github.com/psf/black/issues/1159
+        #
+        "command": "pydocstyle --ignore=D213,D202,D203,D406,D407 python tests",  # TODO : Double-check the ignores
+        "requires": ["pydocstyle-4+<5"],  # TODO : Double-check version number. Should be python-3 enabled
+
+    },
+    "pylint": {
+        "command": "pylint --disable=bad-continuation python/rez_docbot tests",
+        "requires": ["pylint-1.9+<2"],
+    },
+    "unittest_python_2": {
+        "command": "python -m unittest discover",
+        "requires": ["python-2"],
+    },
+    "unittest_python_3": {
+        "command": "python -m unittest discover",
+        "requires": ["python-3"],
+    },
+}
+
 
 def commands():
     import os
 
+    env.PATH.append(os.path.join(root, "bin"))
     env.PYTHONPATH.append(os.path.join(root, "python"))
