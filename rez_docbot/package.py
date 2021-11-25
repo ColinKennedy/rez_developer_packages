e9df2829c880cf9809fd52c1c07e8a52fd8cc1d3
Colin Kennedy <colinvfx@gmail.com>
2021-11-25 06:31:16 -0800

    Adding build-related commands

--- rez_docbot/package.py
+++ rez_docbot/package.py
@@ -6,6 +6,15 @@ description = "Auto build, link, and publish documentation, via Rez"
 
 authors = ["ColinKennedy"]
 
+build_command = "python -m rez_build_helper --items python"
+
+private_build_requires = ["rez_build_helper-1.8+<2"]
+
+requires = [
+    "Sphinx-1.8+<2",
+    "six-1.16+<2",
+]
+
 
 def commands():
     import os
