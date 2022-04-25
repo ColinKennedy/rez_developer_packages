29781dc857024b5e2b94a2be35d55de68aa4b3c2
Colin Kennedy <colinvfx@gmail.com>
2022-04-24 17:14:33 -0700

    Adding test stubs

---
+++ rez_docbot/tests/test_versioning.py
@@ -0,0 +1,33 @@
+"""Make sure all version / latest logic works as expected."""
+
+import unittest
+
+
+class Publish(unittest.TestCase):
+    """Skip / Overwrite documentation, under certain circumstances."""
+
+    def test_any_version_never_skip(self):
+        """Publish documentation for every new version, no matter what.
+
+        .. seealso::
+
+            :doc:`publishing_per_version`
+
+        """
+        raise ValueError()
+
+    def test_skip_version(self):
+        """Don't overwrite the version folder if the Rez package is an older patch."""
+        raise ValueError()
+
+    def test_skip_latest(self):
+        """Don't overwrite the latest folder if the Rez package is an older version."""
+        raise ValueError()
+
+    def test_overwrite_version(self):
+        """Overwrite the version folder if the Rez package is a new patch."""
+        raise ValueError()
+
+    def test_overwrite_latest(self):
+        """Overwrite the latest folder if the Rez package is a new version."""
+        raise ValueError()
