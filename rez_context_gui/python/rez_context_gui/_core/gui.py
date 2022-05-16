eeb4f43256c93474923995fdc89e8828803c2e35
Colin Kennedy <colinvfx@gmail.com>
2022-05-16 00:29:18 -0700

    Getting the GUI to load

--- rez_context_gui/python/rez_context_gui/_core/gui.py
+++ rez_context_gui/python/rez_context_gui/_core/gui.py
@@ -8,17 +8,12 @@ from .qtnodes_extension import gui_node
 
 
 class Widget(QtWidgets.QWidget):
-    def __init__(self, nodes, parent=None):
+    def __init__(self, graph, parent=None):
         super(Widget, self).__init__(parent=parent)
 
-        self._graph = qtnodes.NodeGraphWidget()
-        all_types = {type(node) for node in nodes}
-
-        for class_type in all_types:
-            self._graph.registerNodeClass(gui_node.Node)
-
-        for node in nodes:
-            self._graph.addNode(node)
+        # TODO : Remove this later, since it appears to be unnecessary
+        self.setLayout(QtWidgets.QVBoxLayout())
+        self.layout().addWidget(graph)
 
     @classmethod
     def from_context(cls, context, parent=None):
@@ -28,25 +23,32 @@ class Widget(QtWidgets.QWidget):
 
     @classmethod
     def from_graph(cls, digraph, parent=None):
-        nodes = _to_nodes(digraph)
+        graph = qtnodes.NodeGraphWidget()
+        _add_nodes_to_graph(digraph, graph)
 
-        return cls(nodes, parent=parent)
+        return cls(graph, parent=parent)
 
 
-def _to_nodes(digraph):
+def _add_nodes_to_graph(digraph, graph):
     nodes = []
     table = dict()
 
+    # 1. Create initial nodes
     for node_identifier in digraph.nodes():
         attributes = digraph.node_attributes(node_identifier)
         node_contents = node_schema.Contents.from_rez_graph_attributes(node_identifier, attributes)
         node = gui_node.Node.from_contents(node_contents)
-        nodes.append(node)
+        graph.scene.addItem(node)
         table[node_contents.get_identifier()] = node
 
+    # 2. Register node types
+    all_types = {type(node) for node in nodes}
+
+    for class_type in all_types:
+        graph.registerNodeClass(class_type)
+
+    # 3. Assign knob connections for all of the nodes
     for from_node, to_node in digraph.edges():
         source = table[from_node]
         destination = table[to_node]
         source.knob(constant.INPUT_NAME).connectTo(destination.knob(constant.OUTPUT_NAME))
-
-    return nodes
