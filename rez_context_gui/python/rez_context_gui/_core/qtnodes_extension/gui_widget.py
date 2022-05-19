7531c6731f87f3c861e8e4304164b05a2b95e754
Colin Kennedy <colinvfx@gmail.com>
2022-05-18 20:51:53 -0700

    Adding basic "Expand Selection" functionality

--- rez_context_gui/python/rez_context_gui/_core/qtnodes_extension/gui_widget.py
+++ rez_context_gui/python/rez_context_gui/_core/qtnodes_extension/gui_widget.py
@@ -1,5 +1,6 @@
 from Qt import QtWidgets
 
+from qtnodes import edge as edge_
 import qtnodes
 
 
@@ -9,18 +10,74 @@ class NodeGraphWidget(qtnodes.NodeGraphWidget):
         menu = QtWidgets.QMenu(self)
         action = menu.addAction("Expand Selection")
         action.triggered.connect(self._expand_selection)
+        # TODO : Hide unless it's actually within a conflict
         action = menu.addAction("Expand To Non-Conflict")
         action.triggered.connect(self._expand_to_non_conflict)
 
         menu.exec_(event.globalPos())
 
     def _expand_selection(self):
-        for node in self.scene.selectedItems():
+        selection = self._get_selected_nodes()
+
+        if not selection:
+            QtWidgets.QMessageBox.warning(
+                self,
+                "No Selection",
+                "Please select any node and try again.",
+            )
+
+            return
+
+        expanded = False
+
+        for node in selection:
             for knob in node.knobs():
-                if isinstance(knob, qtnodes.OutputKnob):
-                    node = self.getNodeById(knob.target.uuid)
-                    node.show()
+                for edge in knob.edges:
+                    knob_of_hidden_node = edge.target
+                    hidden_neighbor_node = knob_of_hidden_node.parentItem()
+                    hidden_neighbor_node.show()
+                    expanded = True
+                    edge.show()
+
+                # raise ValueError(knob.edges)
+                # if not isinstance(knob, qtnodes.OutputKnob):
+                #     continue
+                #
+                # for edge in self._get_edges_from_output_knob(knob):
+                #     hidden_neighbor_node = edge.target
+                #     hidden_neighbor_node.show()
+                #     edge.show()
+                #
+                #     expanded = True
+
+        if expanded:
+            return
+
+        QtWidgets.QMessageBox.information(
+            self,
+            "Nothing To Expand",
+            "You've reached the end of the graph. There's nothing new to show.",
+        )
 
     def _expand_to_non_conflict(self):
         # TODO : Add support for this method
-        raise NotImplementedError('Need to write')
+        raise NotImplementedError("Need to write")
+
+    def _get_edges_from_output_knob(self, knob):
+        for item in self.scene.items():
+            print('ITEM', item)
+            if isinstance(item, edge_.Edge):
+                print('SOURCE', item.source, knob)
+
+        return [
+            item
+            for item in self.scene.items()
+            if isinstance(item, edge_.Edge) and item.source == knob
+        ]
+
+    def _get_selected_nodes(self):
+        return [
+            item
+            for item in self.scene.selectedItems()
+            if isinstance(item, qtnodes.Node)
+        ]
